5db82e6635e15e5f2e54a0aa8c9bf6ac244f93b2
Harsh-28 <67831935+Harsh-28@users.noreply.github.com>
2023-04-14 23:32:53 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,5 @@
 let firebaseConfig = {
-   apiKey: "AIzaSyBW4ee_iSQ-VPrY6qT_AHfUuKgXb-KzTtw",
-    authDomain: "blog-c8266.firebaseapp.com",
-    projectId: "blog-c8266",
-    storageBucket: "blog-c8266.appspot.com",
-    messagingSenderId: "420078456230",
-    appId: "1:420078456230:web:ac70ed63cc357d0f219367",
-    measurementId: "G-HEVQE07976"
+   //enter your firebase Config
 };
 
 firebase.initializeApp(firebaseConfig);
